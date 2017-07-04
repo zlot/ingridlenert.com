4dc1b0df747b71d0c8055088f231141d6b66f11f
Mark C Mitchell <mark@markcmitchell.net>
2017-07-04 22:34:54 +1000

    Add Ingrid Lenert title

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1 className='title title--ingrid'>Ingrid</h1>
+          <h1 className='title title--lenert'>Lenert</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
