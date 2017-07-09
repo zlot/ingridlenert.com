80ab05fb8d001471ee1fe1e07040cefba7741bd9
Mark C Mitchell <mark@markcmitchell.net>
2017-07-09 20:41:16 +1000

    webpack-dev: add required dependencies for foundation to work correctly: jquery and script-loader. Add to webpack config to force imports of foundation-sites and jquery to be inserted into global scope

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -173,7 +173,7 @@ module.exports = {
           // directory for faster rebuilds.
           cacheDirectory: true,
         },
-      },
+      },   
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
       // "style" loader turns CSS into JS modules that inject <style> tags.
