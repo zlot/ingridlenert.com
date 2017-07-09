60e1b9923c51f447ff01ef098c78d2a302ad8974
Mark C Mitchell <mark@markcmitchell.net>
2017-07-09 20:08:28 +1000

    Update naming to be more BOM like, update styles

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h1 className='title title--ingrid'>Ingrid</h1>
-          <h1 className='title title--lenert'>Lenert</h1>
+        <div className="header">
+          <div className='header__container'>
+            <h1 className='header__title header__title--ingrid'>Ingrid</h1>
+            <h1 className='header__title header__title--lenert'>Lenert</h1>
+          </div>
         </div>
       </div>
     );
