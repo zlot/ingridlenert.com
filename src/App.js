cb4e910b1970db2cba95c2b1830413a5720b909f
Mark C Mitchell <mark@markcmitchell.net>
2017-07-16 21:19:00 +1000

    add test Lucy animation

--- src/App.js
+++ src/App.js
@@ -8,11 +8,28 @@ export default class App extends Component {
 
   componentDidMount = () => {
     $(document).foundation()
+    let name = `Lucy_Mitsy-`
+    let num = '1'
+    setInterval(() => {
+      if(num > 28) {
+        num = 1
+      }
+      $('#test').removeClass(`Lucy_Mitsy-${num}`)
+      num = pad(Number(num)+1, 2)
+      $('#test').addClass(`Lucy_Mitsy-${num}`)
+    }, 150)
+
+    function pad(num, size) {
+      var s = num+"";
+      while (s.length < size) s = "0" + s;
+      return s;
+    }
   }
 
   render = () => {
     return (
       <div className='App'>
+        <div id='test' className='sprite Lucy_Mitsy-01'></div>
         <Header />
         <div className='grid-container grid-container-padded portfolio'>
           <div className='grid-x grid-padding-x'>
