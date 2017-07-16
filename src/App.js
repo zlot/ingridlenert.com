6f3d811f946183e11f4aaa59043bac78f09ae7e9
Mark C Mitchell <mark@markcmitchell.net>
2017-07-16 21:59:35 +1000

    minor refactor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import './App.scss'
 import Header from './Header'
 import 'foundation-sites'
 import $ from 'jquery'
+import './App.scss'
 
 export default class App extends Component {
 
